0f11e5e763cbfc63a69eea63bac993cc4e100057
Suryansh Yadav <suryanshyadav159@gmail.com>
2025-01-08 19:41:46 +0530

    added some more packages

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const express= require('express')
 const mongoose = require("mongoose");
+const cors=require("cors")
+const app=express();
+const path =require("path")
+require("dotenv").config({path:'./.env'})
+
+
+app.use(cors());
+app.use(express.json())
+
+mongoose.connect(process.env.CONNECTSTRING).then(()=>
+{
+    console.log("database connected")
+}).catch((error)=>
+{
+    console.log("error making connection",error)
+})
